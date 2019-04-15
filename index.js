f4e03abed635e35f36b796753346617668db697d
Claudio <claudioguimaraes14@gmail.com>
2019-04-14 23:15:50 -0300

    Simple index page showing results

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+var secrets = require('./secrets.json')
 var express = require('express');
 var cors = require('cors')
 var querystring = require('querystring');
 var request = require('request')
+const fs = require('fs')
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
    .use(cors())
+app.set('view engine', 'ejs');
 
-var client_id = ''
-var client_secret = ''
+var client_id = secrets.client_id
+var client_secret = secrets.client_secret
 var redirect_uri = 'http://localhost:3000/callback'
-
 app.get('/login', function(req, res){
   // res.send('login')
   // res.redirect('http://globo.com')
-  var scopes = 'user-read-private user-read-email';
+  var scopes = 'user-read-private user-read-email user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' + 
     querystring.stringify({
       response_type: 'code',
@@ -23,11 +25,6 @@ app.get('/login', function(req, res){
       redirect_uri: redirect_uri
     })
   )
-  // res.redirect('https://accounts.spotify.com/authorize' +
-  // '?response_type=code' +
-  // '&client_id=' + my_client_id +
-  // (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  // '&redirect_uri=' + encodeURIComponent(redirect_uri));
 })
 
 app.get('/callback', function(req, res){
@@ -54,13 +51,17 @@ app.get('/callback', function(req, res){
             refresh_token  = body.refresh_token
             console.log(body)
         var options = {
-          url: 'https://api.spotify.com/v1/me',
+          url: 'https://api.spotify.com/v1/me/top/artists',
           headers: { 'Authorization': 'Bearer ' + access_token },
           json: true
         }
-
         request.get(options, function(error, response, body) {
-          console.log(body);
+          var json = JSON.stringify(body.items);
+          fs.writeFileSync('results.json', json, function(err){
+            if(err){
+              return console.log('ERRO')
+            }
+          })
         });
         res.redirect('/?' +
           querystring.stringify({
@@ -74,7 +75,12 @@ app.get('/callback', function(req, res){
 })
 
 app.get('/', function(req, res){
-  res.send('oi')
+  var artists = []
+  var fileBuffer = fs.readFileSync('results.json', 'utf8')
+  var artists = JSON.parse(fileBuffer)
+  res.render('pages/index', {
+    artists: artists
+  })
 })
 
 app.listen(3000, function () {
